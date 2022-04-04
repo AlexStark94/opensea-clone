49bd74c78e39520a83bc3b5d1f2140befe07b320
AlexStark94 <ia.corral94@gmail.com>
2022-04-03 22:59:50 -0300

    Build

---
+++ config.js
@@ -0,0 +1,10 @@
+export const nftaddress = '0x18Bd154608aD26B5c055F27A9652f4975a3Aa979';
+export const nftmarketaddress = '0x152c58111B965bFa8E2Da6174AD682db8a72Cd7a';
+
+// change the above everytime you redeploy your contracts
+
+// command for deploying your contracts on hardhat:
+// npx hardhat run --network locahost scripts/deploy.js
+
+// command for starting or restarting your server:
+// npx hardhat node
